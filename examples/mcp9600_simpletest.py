2de0c7b713f49afff4922481971ee9537dea098c
dherrada <dylan.herrada@gmail.com>
2020-03-15 15:37:19 -0400

    Ran black, updated to pylint 2.x

--- examples/mcp9600_simpletest.py
+++ examples/mcp9600_simpletest.py
@@ -9,9 +9,5 @@ i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 mcp = adafruit_mcp9600.MCP9600(i2c)
 
 while True:
-    print((
-        mcp.ambient_temperature,
-        mcp.temperature,
-        mcp.delta_temperature
-    ))
+    print((mcp.ambient_temperature, mcp.temperature, mcp.delta_temperature))
     time.sleep(1)
